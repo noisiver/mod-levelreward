23515e4e07bd6076186fb41f4587cee5e2bbf3ce
Revision <tkn963@gmail.com>
2021-12-16 18:36:12 +0100

    Fixes
    I renamed the recruit a friend module to refer a friend.
    Renamed the config options to fit the module.

--- src/LevelReward.cpp
+++ src/LevelReward.cpp
@@ -15,42 +15,42 @@ class LevelReward : public PlayerScript
             if (oldlevel < 10 && player->getLevel() == 10)
             {
                 level  = 10;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.10", 5);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.10", 5);
             }
             else if (oldlevel < 20 && player->getLevel() == 20)
             {
                 level  = 20;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.20", 15);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.20", 15);
             }
             if (oldlevel < 30 && player->getLevel() == 30)
             {
                 level  = 30;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.30", 30);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.30", 30);
             }
             else if (oldlevel < 40 && player->getLevel() == 40)
             {
                 level  = 40;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.40", 45);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.40", 45);
             }
             else if (oldlevel < 50 && player->getLevel() == 50)
             {
                 level  = 50;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.50", 60);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.50", 60);
             }
             else if (oldlevel < 60 && player->getLevel() == 60)
             {
                 level  = 60;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.60", 80);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.60", 80);
             }
             else if (oldlevel < 70 && player->getLevel() == 70)
             {
                 level  = 70;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.70", 125);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.70", 125);
             }
             else if (oldlevel < 80 && player->getLevel() == 80)
             {
                 level  = 80;
-                reward = sConfigMgr->GetOption<int32>("Reward.Gold.Level.80", 250);
+                reward = sConfigMgr->GetOption<int32>("LevelReward.Level.80", 250);
             }
 
             if (level > 0 && reward > 0)
